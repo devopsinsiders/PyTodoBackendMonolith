10b39b671c447a9dd1c453117ca071129ff702b5
Ashish Kumar <Ashish.Kumar4.Contractor@pepsico.com>
2023-11-05 01:49:04 +0530

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Use the official Python image as the base image
+FROM python:3.9
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Copy the application files into the container
+COPY . .
+
+# Install necessary packages
+RUN apt-get update && apt-get install -y unixodbc unixodbc-dev
+RUN curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+RUN curl https://packages.microsoft.com/config/debian/10/prod.list > /etc/apt/sources.list.d/mssql-release.list
+RUN apt-get update
+RUN ACCEPT_EULA=Y apt-get install -y msodbcsql17
+
+RUN pip install -r requirements.txt
+
+# Start the FastAPI application
+CMD ["uvicorn", "app:app", "--host", "0.0.0.0", "--port", "8000"]
---
+++ app.py
@@ -0,0 +1,103 @@
+import pyodbc
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+connection_string="Driver={ODBC Driver 18 for SQL Server};Server=tcp:devopsinsidersdbs.database.windows.net,1433;Database=todoapp;UID=devopsadmin;PWD=P@ssw01rd@123;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30"
+
+app = FastAPI()
+
+# Configure CORSMiddleware to allow all origins (disable CORS for development)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # This allows all origins (use '*' for development only)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# Define the Task model
+class Task(BaseModel):
+    title: str
+    description: str
+
+# Create a table for tasks (You can run this once outside of the app)
+@app.get("/")
+def create_tasks_table():
+    try:
+        conn = pyodbc.connect(connection_string)
+        cursor = conn.cursor()
+        cursor.execute("""
+            CREATE TABLE Tasks (
+                ID int NOT NULL PRIMARY KEY IDENTITY,
+                Title varchar(255),
+                Description text
+            );
+        """)
+        conn.commit()        
+    except Exception as e:
+        print(e)
+    return "Table Created... Tasks API Ready"
+
+# List all tasks
+@app.get("/tasks")
+def get_tasks():
+    tasks = []
+    with pyodbc.connect(connection_string) as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM Tasks")
+        for row in cursor.fetchall():
+            task = {
+                "ID": row.ID,
+                "Title": row.Title,
+                "Description": row.Description
+            }
+            tasks.append(task)
+    return tasks
+
+# Retrieve a single task by ID
+@app.get("/tasks/{task_id}")
+def get_task(task_id: int):
+    with pyodbc.connect(connection_string) as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM Tasks WHERE ID = ?", task_id)
+        row = cursor.fetchone()
+        if row:
+            task = {
+                "ID": row.ID,
+                "Title": row.Title,
+                "Description": row.Description
+            }
+            return task
+        return {"message": "Task not found"}
+
+# Create a new task
+@app.post("/tasks")
+def create_task(task: Task):
+    with pyodbc.connect(connection_string) as conn:
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO Tasks (Title, Description) VALUES (?, ?)", task.title, task.description)
+        conn.commit()
+    return task
+
+# Update an existing task by ID
+@app.put("/tasks/{task_id}")
+def update_task(task_id: int, updated_task: Task):
+    with pyodbc.connect(connection_string) as conn:
+        cursor = conn.cursor()
+        cursor.execute("UPDATE Tasks SET Title = ?, Description = ? WHERE ID = ?", updated_task.title, updated_task.description, task_id)
+        conn.commit()
+        return {"message": "Task updated"}
+
+# Delete a task by ID
+@app.delete("/tasks/{task_id}")
+def delete_task(task_id: int):
+    with pyodbc.connect(connection_string) as conn:
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM Tasks WHERE ID = ?", task_id)
+        conn.commit()
+        return {"message": "Task deleted"}
+
+if __name__ == "__main__":
+    create_tasks_table()
