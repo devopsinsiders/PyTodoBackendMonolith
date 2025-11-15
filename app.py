c29fd8b0a28a829b52f5afc864fecd686aa7d0f4
DevOps Insiders <124057238+devopsinsiders@users.noreply.github.com>
2025-11-15 08:53:03 +0530

    Change connection string to placeholder
    Updated connection string placeholder in app.py.

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
-connection_string = os.getenv("CONNECTION_STRING")
+# connection_string = os.getenv("CONNECTION_STRING")
+connection_string = "UPDATE CONNECTION STRING HERE"
 
 app = FastAPI()
 
